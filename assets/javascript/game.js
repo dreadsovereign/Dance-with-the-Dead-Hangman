ce9b584eb03535118978b8d7453ca0b8d67ff931
Kamran <narmak182@gmail.com>
2017-07-09 00:26:13 -0400

    Got a semi working version of the hangman game. Still needs lots of work for calculating guesses and letters used.

---
+++ assets/javascript/game.js
@@ -0,0 +1,66 @@
+var songs = ["andromeda", "waves", "riot", "diabolic", "cobra"];
+
+var word="";
+var answerArray = [];
+
+function chooseSong(){
+  word = songs[Math.floor(Math.random() * songs.length)];
+  answerArray = [];
+  for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+  }
+  document.getElementById("song").innerHTML= answerArray.join(" ");
+  document.getElementById("message").innerHTML= "Press Any Key to Get Started"
+}
+chooseSong();
+
+document.onkeyup = function(event) {
+  var guess = event.key;
+  var win = 0;
+  var i = 0;
+  var guessesLeft = 12;
+
+  document.getElementById("guesses").innerHTML= "Guesses Left: " + guessesLeft;
+  
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] === guess) {
+      answerArray[i] = guess;
+    }
+  }
+  var remaining_letters = 0;
+  for (i = 0; i < word.length; i++) {
+    if (answerArray[i] === "_") {
+      remaining_letters += 1;
+    }
+  }
+
+  // Letters Already Guessed
+
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] != guess) {
+      guessesLeft--;
+      document.getElementById("guesses").innerHTML= "Guesses Left: " + guessesLeft;
+    }
+  }
+
+  // Reset after player wins
+
+  if (remaining_letters == 0) {
+    word = songs[Math.floor(Math.random() * songs.length)];
+    answerArray = [];
+    for (var i = 0; i < word.length; i++) {
+      answerArray[i] = "_";
+      win++;
+      document.getElementById("wins").innerHTML= "Wins: " + win;
+    }
+  }
+
+  document.getElementById("song").innerHTML= answerArray.join(" ");
+
+
+}
+
+
+
+
+
